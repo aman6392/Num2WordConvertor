23f67e69cc8e517bd3d2d9e2e343408118edde45
mahajanaman <aman.mahajan@uwa.edu.au>
2021-08-31 14:48:35 +0800

    intial commit

---
+++ CurrencyToWords/CurrencyToWords/NumToWords.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace CurrencyToWords
+{
+    partial class numToWords
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNum = new System.Windows.Forms.TextBox();
+            this.labelNum = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.labelHeading = new System.Windows.Forms.Label();
+            this.labelCharDisp = new System.Windows.Forms.Label();
+            this.labelLimitations = new System.Windows.Forms.Label();
+            this.labelError = new System.Windows.Forms.Label();
+            this.labelErrorText = new System.Windows.Forms.Label();
+            this.textBoxCharDisplay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxNum
+            // 
+            this.textBoxNum.Location = new System.Drawing.Point(164, 81);
+            this.textBoxNum.MaxLength = 14;
+            this.textBoxNum.Name = "textBoxNum";
+            this.textBoxNum.Size = new System.Drawing.Size(287, 20);
+            this.textBoxNum.TabIndex = 1;
+            this.textBoxNum.TextChanged += new System.EventHandler(this.textBoxNum_TextChanged);
+            // 
+            // labelNum
+            // 
+            this.labelNum.AutoSize = true;
+            this.labelNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNum.Location = new System.Drawing.Point(12, 82);
+            this.labelNum.Name = "labelNum";
+            this.labelNum.Size = new System.Drawing.Size(146, 16);
+            this.labelNum.TabIndex = 1001;
+            this.labelNum.Text = "Please enter a number:";
+            this.labelNum.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(164, 328);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(269, 328);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(376, 328);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // labelHeading
+            // 
+            this.labelHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeading.ForeColor = System.Drawing.Color.Navy;
+            this.labelHeading.Location = new System.Drawing.Point(63, 9);
+            this.labelHeading.Name = "labelHeading";
+            this.labelHeading.Size = new System.Drawing.Size(369, 29);
+            this.labelHeading.TabIndex = 1000;
+            this.labelHeading.Text = "Hexagon Mining: Cheque Writing Program";
+            this.labelHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelCharDisp
+            // 
+            this.labelCharDisp.AutoSize = true;
+            this.labelCharDisp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCharDisp.Location = new System.Drawing.Point(12, 147);
+            this.labelCharDisp.Name = "labelCharDisp";
+            this.labelCharDisp.Size = new System.Drawing.Size(124, 16);
+            this.labelCharDisp.TabIndex = 1002;
+            this.labelCharDisp.Text = "Converted Number:";
+            this.labelCharDisp.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // labelLimitations
+            // 
+            this.labelLimitations.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelLimitations.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLimitations.Location = new System.Drawing.Point(12, 394);
+            this.labelLimitations.Name = "labelLimitations";
+            this.labelLimitations.Size = new System.Drawing.Size(439, 93);
+            this.labelLimitations.TabIndex = 1006;
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelError.Location = new System.Drawing.Point(12, 257);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(55, 16);
+            this.labelError.TabIndex = 1003;
+            this.labelError.Text = "Error(s):";
+            // 
+            // labelErrorText
+            // 
+            this.labelErrorText.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelErrorText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelErrorText.Location = new System.Drawing.Point(161, 256);
+            this.labelErrorText.Name = "labelErrorText";
+            this.labelErrorText.Size = new System.Drawing.Size(290, 38);
+            this.labelErrorText.TabIndex = 1005;
+            // 
+            // textBoxCharDisplay
+            // 
+            this.textBoxCharDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxCharDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCharDisplay.Location = new System.Drawing.Point(161, 147);
+            this.textBoxCharDisplay.Multiline = true;
+            this.textBoxCharDisplay.Name = "textBoxCharDisplay";
+            this.textBoxCharDisplay.ReadOnly = true;
+            this.textBoxCharDisplay.Size = new System.Drawing.Size(287, 93);
+            this.textBoxCharDisplay.TabIndex = 1004;
+            // 
+            // numToWords
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 499);
+            this.Controls.Add(this.textBoxCharDisplay);
+            this.Controls.Add(this.labelErrorText);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.labelLimitations);
+            this.Controls.Add(this.labelCharDisp);
+            this.Controls.Add(this.labelHeading);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.labelNum);
+            this.Controls.Add(this.textBoxNum);
+            this.Name = "numToWords";
+            this.Text = "Cheque Writing Program";
+            this.Load += new System.EventHandler(this.numToWords_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNum;
+        private System.Windows.Forms.Label labelNum;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label labelHeading;
+        private System.Windows.Forms.Label labelCharDisp;
+        private System.Windows.Forms.Label labelLimitations;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Label labelErrorText;
+        private System.Windows.Forms.TextBox textBoxCharDisplay;
+    }
+}
+
---
+++ CurrencyToWords/CurrencyToWords/NumToWords.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyToWords
+{
+    public partial class numToWords : Form
+    {
+        public numToWords()
+        {
+            InitializeComponent();            
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //Exit from the application
+            Application.Exit();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Clear button functionality, Clear the text from the label and display the default value in the input textbox
+            textBoxNum.Text = "0.00";
+            textBoxCharDisplay.Text = "";
+            labelErrorText.Text = "";
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            String Excptn = "";
+            String Words1 = "";
+            String Words2 = "";
+            String inpNumStr = "";
+            Decimal inpNum;
+            Int64 bfrDec = 0;
+            Int64 aftrDec = 0;
+            String dol = " DOLLAR AND ";
+            String dols = " DOLLARS AND ";
+
+            try
+            {
+                //Took the number in string format as per the requirement
+                inpNumStr = textBoxNum.Text;
+
+                //used regex to make sure only valid number comes into the application for processing
+                String regex = @"^\d+\.{0,1}\d*$";
+
+                Regex reg = new Regex(regex);
+                if (reg.IsMatch(inpNumStr))
+                {
+                    //Round the number upto 2 decimal places only
+                    inpNum = Math.Round(Decimal.Parse(textBoxNum.Text),2);
+
+                    //converting number back to string to calculate the location of the decimal point
+                    inpNumStr = inpNum.ToString();
+                    int loc = inpNumStr.IndexOf(".");
+
+                    if (loc < 0)
+                    {
+                        bfrDec = Int64.Parse(inpNumStr);
+                        if (bfrDec == 0)
+                        {
+                            Words1 = "ZERO DOLLAR AND";
+                        }
+                        else
+                        {
+                            if (bfrDec < 10)
+                            {
+                                Words1 = ConvertNum(bfrDec) + dol;
+                            }
+                            else
+                            {
+                                Words1 = ConvertNum(bfrDec) + dols;
+                            }
+                            
+                        }
+                        textBoxCharDisplay.Text = Words1 + " ZERO CENTS";
+                    }
+                    else if (loc == 0)
+                    {
+                        aftrDec = Int16.Parse(inpNumStr.Substring(loc + 1));
+                        
+                        Words1 = ConvertNum(bfrDec);
+                        Words2 = ConvertNum(aftrDec);
+                        textBoxCharDisplay.Text = (Words1 + Words2 + " CENTS");
+                    }
+                    else if (loc > 0)
+                    {
+                        bfrDec = Int64.Parse(inpNumStr.Substring(0,loc));
+                        aftrDec = Int16.Parse(inpNumStr.Substring(loc + 1));
+                        if (bfrDec == 0 && aftrDec == 0)
+                        {
+                            Words1 = "ZERO DOLLAR AND ";
+                            Words2 = "ZERO";
+                            textBoxCharDisplay.Text = (Words1 + Words2 + " CENTS");
+                        }
+                        else if(aftrDec!= 0 && bfrDec != 0)
+                        {
+                            if (bfrDec < 10)
+                            {
+                                Words1 = ConvertNum(bfrDec) + dol;
+                            }
+                            else
+                            {
+                                Words1 = ConvertNum(bfrDec) + dols;
+                            }
+                            Words2 = ConvertNum(aftrDec);
+                            textBoxCharDisplay.Text = (Words1 + Words2 + " CENTS");
+                        }
+                        else if (bfrDec == 0 && aftrDec != 0)
+                        {
+                            Words1 = "ZERO DOLLAR AND ";
+                            Words2 = ConvertNum(aftrDec);
+                            textBoxCharDisplay.Text = (Words1 + Words2 + " CENTS");
+                        }
+                        else if (bfrDec != 0 && aftrDec == 0)
+                        {
+                            if (bfrDec < 10)
+                            {
+                                Words1 = ConvertNum(bfrDec) + dol;
+                            }
+                            else
+                            {
+                                Words1 = ConvertNum(bfrDec) + dols;
+                            }
+                            Words2 = "ZERO";
+                            textBoxCharDisplay.Text = (Words1 + Words2 + " CENTS");
+                        }
+                    }
+                }
+                else
+                {
+                    labelErrorText.Text = "Please Check the number you have entered!!";
+                    
+                }
+            }
+            catch(FormatException fe)
+            {
+                Excptn = fe.Message;
+                textBoxCharDisplay.Text = Excptn+"test";
+            }
+
+        }
+
+        public String ConvertNum(long num)
+        {
+            String strFinal = "";
+
+            String[] ones = {   "", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX",
+                                "SEVEN", "EIGHT", "NINE", "TEN", "ELEVEN", "TWELVE",
+                                "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN",
+                                "EIGHTEEN", "NINETEEN" };
+
+            String[] tens = {"","","TWENTY", "THIRTY", "FOURTY", "FIFTY", "SIXTY", "SEVENTY", "EIGHTY", "NINETY"};
+
+            /*
+            To convert input number to string i am doing it using input number length approach (created by me) because the input number length will be same as the length of the denominator. 
+            For example: if an input number is 303.33, then before decimal has length 3 so will get divided by 100 and after decimal part has length of 2
+            so it will go to case 2 and get divided by 10.
+            */
+
+            string s = num.ToString();
+            int len = s.Length;
+
+            switch (len)
+            {
+                case (1):                                                   //ones
+                    strFinal = ones[num];
+                    break;
+                case (2) when (num >= 10 && num < 100):                     //tens
+                    if (num >= 10 && num < 20)
+                    {
+                        strFinal = ones[num];
+                    }
+                    else
+                    {
+                        strFinal = tens[num / 10] + ones[num % 10];
+                    }
+                    break; 
+                case 3:                                                     //hundreds
+                    strFinal = ones[num / 100] + " HUNDRED " + ConvertNum(num % 100);
+                    break;
+                case 4:                                                     //thousands
+                    strFinal = ConvertNum(num / 1000) + " THOUSAND " + ConvertNum(num % 1000);
+                    break;
+                case 5:                                                     //ten thousands
+                    goto case 4;
+                case 6:                                                     //hundred thousands
+                    goto case 4;
+                case 7:                                                     //million
+                    strFinal = ConvertNum(num / 1000000) + " MILLION " + ConvertNum(num % 1000000);
+                    break;
+                case 8:                                                     //ten million
+                    goto case 7;
+                case 9:                                                     //hundred million
+                    goto case 7;
+                default:
+                    labelErrorText.Text = "Please input a number less than a billion";
+                    textBoxCharDisplay.Text = "";
+                    strFinal = "";
+                    break;
+            }        
+
+                return strFinal;
+        }
+
+            private void numToWords_Load(object sender, EventArgs e)
+        {
+            //Display application Limitations
+            labelLimitations.Text = "Application Limitation(s):\r\n" +
+                                    "1. This application is only designed to convert numbers less than a billion. \r\n" +
+                                    "2. This application convert number value to words as per Australian Currency Standards.";
+
+            //On form load, display default values of the following.
+            textBoxNum.Text = "0.00";
+            labelErrorText.Text = "";
+            textBoxCharDisplay.Text = "";
+        }
+
+        private void textBoxNum_TextChanged(object sender, EventArgs e)
+        {
+            textBoxCharDisplay.Text = "";
+            labelErrorText.Text = "";
+        }
+    }
+}
